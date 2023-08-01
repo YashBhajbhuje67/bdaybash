17ebf2c1ebbc20da3700c31f9a023e6fe07030bf
YashBhajbhuje67 <yashbhajbhuje67@gmail.com>
2023-08-01 17:21:18 +0530

    created files

---
+++ src/components/Counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GiftBox from './GiftBox'
+
+const Counter = () => {
+  return (
+    <div>
+      <GiftBox/>
+    </div>
+  )
+}
+
+export default Counter
---
+++ src/components/GiftBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FinalPage from './FinalPage'
+
+const GiftBox = () => {
+  return (
+    <div>
+      <FinalPage/>
+    </div>
+  )
+}
+
+export default GiftBox
