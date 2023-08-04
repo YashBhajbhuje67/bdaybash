737153a46aa596d3e58239bce6180c563950a29e
YashBhajbhuje67 <yashbhajbhuje67@gmail.com>
2023-08-04 22:31:38 +0530

    redirect to main count down page

--- src/components/FinalPage.jsx
+++ src/components/FinalPage.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
-const FinalPage = () => {
+const FinalPage = ({ countDownDone }) => {
+  
+  const nav = useNavigate();
+
+  useEffect(()=>{
+    if (! countDownDone){
+      nav('/');
+    }
+  },[])
   return (
     <div>
       <div>Happy Birth Day</div>
